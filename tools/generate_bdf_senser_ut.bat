2420774b5814acbe33e2af7a5a8b0c06d35136f4
yosuzuki <yosuzuki@techmatorix.co.jp>
2025-03-11 17:58:14 +0900

    a

---
+++ tools/generate_bdf_senser_ut.bat
@@ -0,0 +1,10 @@
+set BUILD_TOOL_CHAIN_DIR=C:\cygwin64
+set CPPTEST_DIR=C:\Parasoft\20232\cpptest
+set PATH=%BUILD_TOOL_CHAIN_DIR%\bin;%CPPTEST_DIR%\bin;%PATH%
+
+set WORKSPACE_DIR=%~dp0..\
+set PROJECT_NAME=Sensor
+set PROJECT_DIR=%WORKSPACE_DIR%\%PROJECT_NAME%
+
+cd %PROJECT_DIR%
+cpptesttrace make
