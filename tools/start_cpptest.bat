91c9aa948662289f0260f7046bc177753b529537
yosuzuki <yosuzuki@techmatorix.co.jp>
2026-02-26 11:36:30 +0900

    a

--- tools/start_cpptest.bat
+++ tools/start_cpptest.bat
@@ -2,4 +2,4 @@ cd /d "%~dp0"
 set BUILD_TOOL_CHAIN_DIR=C:\cygwin64
 set CPPTEST_DIR=C:\Parasoft\20252\cpptest
 set PATH=%BUILD_TOOL_CHAIN_DIR%\bin;%CPPTEST_DIR%;%PATH%
-cpptest -clean -data %~dp0..\ -J-Xms512m -J-Xmx4096m
+cpptest -clean -data %~dp0..\cpptest_workspace -J-Xms512m -J-Xmx4096m
