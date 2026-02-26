b540e820e3db5e8474f24850c1c87adbc3f8293b
yosuzuki <yosuzuki@techmatorix.co.jp>
2026-02-26 12:01:49 +0900

    a

---
+++ cpptest_workspace/cpptestcli_exec_sensor_ut.bat
@@ -0,0 +1,16 @@
+set BUILD_TOOL_CHAIN_DIR=C:\cygwin64
+set CPPTEST_DIR=C:\Parasoft\20252\cpptest
+set PATH=%BUILD_TOOL_CHAIN_DIR%\bin;%CPPTEST_DIR%;%PATH%
+
+set WORKSPACE_DIR=%~dp0
+set PROJECT_NAME=Sensor
+set PROJECT_DIR=%WORKSPACE_DIR%\%PROJECT_NAME%
+
+cpptestcli ^
+-data %WORKSPACE_DIR% ^
+-bdf %WORKSPACE_DIR%\%PROJECT_NAME%\cpptestscan.bdf ^
+-resource %PROJECT_NAME% ^
+-config "builtin://Run Unit Tests (File Scope)" ^
+-report %PROJECT_DIR%\report_ut ^
+-showdetails ^
+-appconsole stdout
